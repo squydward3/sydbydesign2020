a4a3ea31475dda3ae5fbfa9d4aaca321fe233c73
Sydney Mai <apm417@nyu.edu>
2019-12-24 17:01:41 -0500

    Added Design at Abacus

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,14 +1,27 @@
 $(document).ready(function() {
 
+  // jump to footer
   $(".nav-work").click(function() {
     $([document.documentElement, document.body]).animate({
         scrollTop: $("#footer").offset().top
     }, 700);
   });
+
+  // jump to first project
   $(".nav-work-index").click(function() {
     $([document.documentElement, document.body]).animate({
         scrollTop: $("#monthly-recap").offset().top
     }, 500);
+  });
+
+  // make fixed nav
+  $(window).bind('scroll', function () {
+    var x = $('.nav-work').parents('nav')
+    if ($(window).scrollTop() > 108) {
+        x.addClass('fixed');
+    } else {
+        x.removeClass('fixed');
+    }
   });
 
 })
